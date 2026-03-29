399cb084816a38eb0d5aa2f9fae31a8f79a39b47
MonkeyOnKeyboard-mok <santi.ortega.serra@gmail.com>
2026-03-29 15:27:59 -0300

    first

---
+++ script_templates/Node/basic_custom_script.gd
@@ -0,0 +1,23 @@
+# meta-name: Base Custom Script
+# meta-description: Base script with structures 
+# meta-default: true
+# meta-space-indent: 4
+extends Node
+## enums
+## consts
+## exports
+## public vars
+## private vars
+## onready vars
+# "obj_" for node references;
+## built-in override methods
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	pass
+
+## public methods
+
+## private methods
